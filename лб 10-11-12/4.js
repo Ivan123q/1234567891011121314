4fd0563e609224718d57f481a17998609a38bc12
Ivan123q <151920930+Ivan123q@users.noreply.github.com>
2024-07-01 11:46:59 +0300

    Add files via upload

---
+++ лб 10-11-12/4.js
@@ -0,0 +1,28 @@
+﻿function calculateY(x) {
+    try {
+        if (x > 3 || x < -3) {
+            throw new Error("Недопустимое значение x: корень из отрицательного числа невозможен.");
+        }
+
+        let y = 2 - Math.sqrt(9 - x ** 2);
+        return y;
+    } catch (error) {
+        alert(error.message);
+        return null;
+    }
+}
+
+function displayResults() {
+    const resultsDiv = document.getElementById("results");
+    const xValues = [0, 1, 2, 3, 4, -4];
+    xValues.forEach(x => {
+        const result = calculateY(x);
+        if (result !== null) {
+            resultsDiv.innerHTML += `Для x = ${x}, y = ${result.toFixed(2)}<br>`;
+        } else {
+            resultsDiv.innerHTML += `Для x = ${x} расчет невозможен.<br>`;
+        }
+    });
+}
+
+window.onload = displayResults;
---
+++ лб 10-11-12/5.js
@@ -0,0 +1,25 @@
+﻿window.onload = function () {
+    // Задаем строковые переменные
+    let s1 = "Я люблю Беларусь";
+    let s2 = "Я учусь в Политехническом колледже";
+
+    // 1. Определяем длину строки s1
+    let lengthS1 = s1.length;
+
+    // Определяем номер варианта (n). Для примера возьмем n = 5.
+    let n = 5;
+    let nthChar = s1[n - 1]; // Индексация начинается с 0, поэтому берем n-1
+    let nthCharCode = nthChar.charCodeAt(0);
+
+    // 3. Заменяем в строке s2 все буквы "о" на "а"
+    let replacedS2 = s2.replace(/о/g, "а");
+
+    // Отображаем результаты
+    let resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = `
+        <p>Длина строки s1: ${lengthS1}</p>
+        <p>${n}-ый символ в строке s1: ${nthChar}</p>
+        <p>ASCII-код ${n}-ого символа в строке s1: ${nthCharCode}</p>
+        <p>Строка s2 после замены всех "о" на "а": ${replacedS2}</p>
+    `;
+};
